8ce7cf85e55c131a0e1e8df490159ddc94c3b6ee
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-02 07:47:19 +0600

    add facebook provider

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,20 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const login = () => {
-    return (
-        <div>
-            <h2 className="text-2xl"> Login Page </h2>
-            <Link href='/' > Back to Home page </Link>
+  return (
+      <div className="card w-96 mx-auto bg-base-100 shadow-xl">
+        <div className="card-body">
+          <Link href="/"> Back to Home page </Link>
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-center">
+            <button className="btn">Login With Google</button>
+            <button className="btn"> Login With Github </button>
+            <button className="btn">Login wigh Facebook </button>
+          </div>
         </div>
-    );
+      </div>
+  );
 };
 
 export default login;
